9007633cd57910cbff960983ff907f8908d22a4b
Joshua Achorn <superp00t@tutanota.com>
2022-11-07 23:03:11 -0500

    feat(Range): add range implementations

---
+++ tempest/range/CRange.hpp
@@ -0,0 +1,12 @@
+#ifndef TEMPEST_RANGE_C_RANGE_HPP
+#define TEMPEST_RANGE_C_RANGE_HPP
+
+#include <cstdint>
+
+class CRange {
+    public:
+    float l;
+    float h;
+};
+
+#endif
---
+++ tempest/range/CiRange.hpp
@@ -0,0 +1,12 @@
+#ifndef TEMPEST_RANGE_C_IRANGE_HPP
+#define TEMPEST_RANGE_C_IRANGE_HPP
+
+#include <cstdint>
+
+class CiRange {
+    public:
+    int32_t l;
+    int32_t h;
+};
+
+#endif
